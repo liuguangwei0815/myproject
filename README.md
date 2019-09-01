44a1cb6dd2df86574aafe670ed9f992e19eb2d7b
lw <736873792@qq.com>
2019-09-01 21:20:27 +0800

    添加bus配置 未完成

--- README.md
+++ README.md
@@ -2,8 +2,14 @@
 #Zuul 是暴露的路由
 #eureka 是注册中心
 #配置本地host
-#添加eureka配置
+
+#注册中心
 127.0.0.1 eureka1
 127.0.0.1 eureka2
 127.0.0.1 eureka3
-
+#redis
+192.168.2.212 redis.com
+#activemq
+192.168.2.212 activemq.com
+#mysql
+192.168.2.212 mysql.com
--- accout-service/src/main/java/com/quicky/demo/controller/UserController.java
+++ accout-service/src/main/java/com/quicky/demo/controller/UserController.java
@@ -1,18 +1,22 @@
 package com.quicky.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
-import com.quicky.demo.model.User;
 import com.quicky.demo.service.UserService;
 import com.quicky.licm.model.UserInfo;
 
 @Controller
 @Scope("prototype")
+//这里面的属性有可能会更新的，git中的配置中心变化的话就要刷新，没有这个注解内，配置就不能及时更新
+@RefreshScope
 public class UserController {
 
 	@Autowired
@@ -41,6 +45,21 @@ public class UserController {
 		return JSONArray.toJSONString(userService.createUser());
 	}
 	
+	@Value("${name1}")
+    private String name;
+    @Value("${password1}")
+    private String password;
+    @Value("${age}")
+    private String age;
+    /**
+     * 通过配置中心获取参数
+     * @return
+     */
+	@RequestMapping("/getConfigVal")
+	@ResponseBody
+	public String getConfigVal() {
+		return JSON.toJSONString(name+":"+password+":"+age);
+	}
 	
 	
 }
