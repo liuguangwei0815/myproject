b62ec69ba48a4c7307c34437a45aced2fa8e55a7
刘威 <liuwei@yiqiandai.com>
2019-09-04 15:31:03 +0800

    oauth2认证

---
+++ my-auth/src/main/java/com/quicky/demo/controller/TestController.java
@@ -0,0 +1,57 @@
+package com.quicky.demo.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.fastjson.JSON;
+
+@RestController
+@RequestMapping(value = "/")
+public class TestController {
+
+	@RequestMapping(value = "order/demo")
+	public YYModel getDemo() {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		System.out.println(auth);
+		YYModel yy = new YYModel();
+		yy.setYy("中文");
+		yy.setZz(3);
+		return yy;
+	}
+
+	@GetMapping("/test")
+	public String getTest() {
+		YYModel yy = new YYModel();
+		yy.setYy("中文");
+		yy.setZz(3);
+		return JSON.toJSONString(yy);
+	}
+
+	static class YYModel {
+
+		String yy;
+
+		Integer zz;
+
+		public String getYy() {
+			return yy;
+		}
+
+		public void setYy(String yy) {
+			this.yy = yy;
+		}
+
+		public Integer getZz() {
+			return zz;
+		}
+
+		public void setZz(Integer zz) {
+			this.zz = zz;
+		}
+
+	}
+
+}
