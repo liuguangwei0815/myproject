8660b6a04d545e8a961eafa59af61f21e878c512
刘威 <liuwei@yiqiandai.com>
2019-09-06 11:21:53 +0800

    没有security 的单点登录

--- my-zuul/src/main/java/com/quicky/demo/filter/CsrfTokenFilter.java
+++ my-zuul/src/main/java/com/quicky/demo/filter/CsrfTokenFilter.java
@@ -1,35 +1,35 @@
-package com.quicky.demo.filter;
-
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-
-/**
- * form中添加CSRF的hidden字段
- * 
- * <input name="${(_csrf.parameterName)!}" value="${(_csrf.token)!}" type=
- * "hidden"> ajax中添加CSRF的头
- * 
- * xhr.setRequestHeader("${_csrf.headerName}", "${_csrf.token}");
- * 
- * 仿跨域攻击
- * 
- * @author Administrator
- *
- */
-@Configuration
-public class CsrfTokenFilter extends WebMvcConfigurationSupport {
-
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	@Bean
-	public FilterRegistrationBean csrfFilter() {
-		FilterRegistrationBean registration = new FilterRegistrationBean();
-		registration.setFilter(new CsrfFilter(new HttpSessionCsrfTokenRepository()));
-		registration.addUrlPatterns("/*");
-		return registration;
-	}
-
-}
+//package com.quicky.demo.filter;
+//
+//import org.springframework.boot.web.servlet.FilterRegistrationBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.web.csrf.CsrfFilter;
+//import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+//
+///**
+// * form中添加CSRF的hidden字段
+// * 
+// * <input name="${(_csrf.parameterName)!}" value="${(_csrf.token)!}" type=
+// * "hidden"> ajax中添加CSRF的头
+// * 
+// * xhr.setRequestHeader("${_csrf.headerName}", "${_csrf.token}");
+// * 
+// * 仿跨域攻击
+// * 
+// * @author Administrator
+// *
+// */
+//@Configuration
+//public class CsrfTokenFilter extends WebMvcConfigurationSupport {
+//
+//	@SuppressWarnings({ "rawtypes", "unchecked" })
+//	@Bean
+//	public FilterRegistrationBean csrfFilter() {
+//		FilterRegistrationBean registration = new FilterRegistrationBean();
+//		registration.setFilter(new CsrfFilter(new HttpSessionCsrfTokenRepository()));
+//		registration.addUrlPatterns("/*");
+//		return registration;
+//	}
+//
+//}
