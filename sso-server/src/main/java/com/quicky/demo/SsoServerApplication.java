348c25e1c2cb2572d0195ed9b1f12b6856404850
lw <736873792@qq.com>
2019-09-17 23:05:53 +0800

    fix

--- sso-server/src/main/java/com/quicky/demo/SsoServerApplication.java
+++ sso-server/src/main/java/com/quicky/demo/SsoServerApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 public class SsoServerApplication {
 	public static void main(String[] args) {
+		
 		SpringApplication.run(SsoServerApplication.class, args);
 	}
 }
