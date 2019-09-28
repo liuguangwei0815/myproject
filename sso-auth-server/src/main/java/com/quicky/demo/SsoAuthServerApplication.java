44658366908e9b8a5dd16f6de92c12cdd2c4616d
刘威 <liuwei@yiqiandai.com>
2019-09-28 11:09:29 +0800

    fix

---
+++ sso-auth-server/src/main/java/com/quicky/demo/SsoAuthServerApplication.java
@@ -0,0 +1,11 @@
+package com.quicky.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoAuthServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SsoAuthServerApplication.class, args);
+    }
+}
